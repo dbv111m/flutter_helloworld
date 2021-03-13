2c41985e54a1560f5e849453bb8d19ccee88590b
Dan <dbv111m@gmail.com>
2021-03-13 03:22:35 +0300

    Добавлены запятые для форматирования кода

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      title: Text("Hello world"),
-    ),
-    body: Center(
-        child: Text("Flutter my demo",
+        appBar: AppBar(
+          title: Text("Hello world"),
+        ),
+        body: Center(
+          child: Text(
+            "Flutter my demo",
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
-            ))),
-  )));
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
